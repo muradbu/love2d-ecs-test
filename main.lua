dd91c3e2f140a420de607a47a1a93379d5aaea8c
muradbuyukasik <10285764+muradbuyukasik@users.noreply.github.com>
2024-06-07 00:49:35 +0200

    init

---
+++ main.lua
@@ -0,0 +1,26 @@
+function love.load()
+end
+
+function love.update(dt)
+end
+
+function love.draw()
+end
+
+function love.mousepressed()
+end
+
+function love.mousereleased()
+end
+
+function love.keypressed()
+end
+
+function love.keyreleased()
+end
+
+function love.focus()
+end
+
+function love.quit()
+end
